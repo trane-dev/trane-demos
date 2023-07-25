b1bc0dadf5a5c701a7fc0e4ecf20e7f820acbb35
Patrik Đurđević <patrik.durdevic@gmail.com>
2023-07-24 22:04:42 -0400

    Update Instacart (Abstracted some functionality into utils)

---
+++ instacart/baytune_util.py
@@ -0,0 +1,75 @@
+from sklearn.datasets import load_wine
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import roc_auc_score, make_scorer
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+import lightgbm as lgb
+import xgboost as xgb
+import warnings
+
+from btb.tuning import Tunable
+from btb.tuning import hyperparams as hp
+
+def automl(X, y):
+    models = {
+        'LGB': lgb.LGBMClassifier,
+        'XGB': xgb.XGBClassifier,
+        'DTC': DecisionTreeClassifier,
+        'SGDC': SGDClassifier,
+        'RF': RandomForestClassifier,
+    }
+
+    def scoring_function(model_name, hyperparameter_values):
+        model_class = models[model_name]
+        model_instance = model_class(**hyperparameter_values)
+        skf = StratifiedKFold(n_splits=5)
+        scores = cross_val_score(
+            estimator=model_instance,
+            X=X,
+            y=y,
+            scoring=make_scorer(roc_auc_score),
+            cv=skf,
+            n_jobs=-1
+        )
+        return scores.mean()
+
+    tunables = {
+        'LGB': Tunable({
+            'num_leaves': hp.IntHyperParam(min=2, max=100),
+            'max_depth': hp.IntHyperParam(min=3, max=200),
+            'learning_rate': hp.FloatHyperParam(min=0.01, max=1),
+            'n_estimators': hp.IntHyperParam(min=10, max=1000),
+        }),
+        'XGB': Tunable({
+            'max_depth': hp.IntHyperParam(min=3, max=200),
+            'learning_rate': hp.FloatHyperParam(min=0.01, max=1),
+            'n_estimators': hp.IntHyperParam(min=10, max=1000),
+        }),
+        'DTC': Tunable({
+            'max_depth': hp.IntHyperParam(min=3, max=200),
+            'min_samples_split': hp.FloatHyperParam(min=0.01, max=1)
+        }),
+        'SGDC': Tunable({
+            'max_iter': hp.IntHyperParam(min=1, max=5000, default=1000),
+            'tol': hp.FloatHyperParam(min=1e-3, max=1, default=1e-3),
+        }),
+        'RF': Tunable({
+            'n_estimators': hp.IntHyperParam(min=10, max=1000),
+            'max_depth': hp.IntHyperParam(min=3, max=200),
+            'min_samples_split': hp.FloatHyperParam(min=0.01, max=1),
+        }),
+    }
+
+    from btb import BTBSession
+
+    session = BTBSession(
+        tunables=tunables,
+        scorer=scoring_function,
+        verbose=True
+    )
+
+    best_proposal = session.run(10)
+
+    return best_proposal
