a93014f98e4ae149843469524ce767e57f71fa93
Patrik Đurđević <patrik.durdevic@gmail.com>
2023-07-26 14:39:44 -0400

    Yelp Reviews Demo Update

--- yelp_reviews/util.py
+++ yelp_reviews/util.py
@@ -15,22 +15,12 @@ def load_data():
         "review": (pd.read_json("data/yelp_academic_dataset_review.json", lines=True), "id"),
         "user": (pd.read_json("data/yelp_academic_dataset_user.json", lines=True), "id"),
     }
-
-    def votes_json_to_cols(votes):
-        return pd.Series([votes["funny"], votes["useful"], votes["cool"]], index=["votes_funny", "votes_useful", "votes_cool"])
-
-    dataframes["review"] = (pd.concat([dataframes["review"][0].drop("votes", axis=1), dataframes["review"][0]["votes"].apply(votes_json_to_cols)], axis=1), "id")
-    dataframes["user"] = (pd.concat([dataframes["user"][0].drop("votes", axis=1), dataframes["user"][0]["votes"].apply(votes_json_to_cols)], axis=1), "id")
-
-    keys = []
-    for h in range(24):
-        for d in range(7):
-            keys.append(str(h) + "-" + str(d))
-
-    def checkin_info_json_to_cols(checkin_info):
-        return pd.Series([checkin_info[k] if k in checkin_info else 0 for k in keys], index=keys)
-    dataframes["checkin"] = (pd.concat([dataframes["checkin"][0].drop("checkin_info", axis=1), dataframes["checkin"][0]["checkin_info"].apply(checkin_info_json_to_cols)], axis=1), "id")
     
+    dataframes["checkin"] = (
+        dataframes["checkin"][0][['type', 'business_id']],
+        "id"
+    )
+
     relationships = [
         ("user", "user_id", "review", "user_id"),
         ("business", "business_id", "review", "business_id"),
